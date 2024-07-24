403cea21926553363e41486f701cb56d66aaa197
Nisarg Limbachiya <84617562+Nisarg077@users.noreply.github.com>
2024-07-24 13:42:55 +0530

    some responsiveness for admin page
    responsiveness and some fixes are done.

--- includes/topheader.php
+++ includes/topheader.php
@@ -1,18 +1,23 @@
 <div class="flex items-center justify-between bg-gray-800 p-4">
     <!-- LOGO -->
-    <div class="flex items-center">
+    <div class="flex justify-start items-center">
         <a href="index.html" class="text-white text-2xl font-bold flex items-center">
             <span>NEWS<span class="text-yellow-500"> EVERYDAY</span></span>
             <i class="mdi mdi-layers ml-2"></i>
         </a>
+        <button id="toggleButton" class=" p-2 border border-transparent ml-3 rounded hover:bg-gray-800 focus:outline-none">
+            <i class="fa-solid fa-bars text-white text-2xl"></i>
+        </button>
+
+        
     </div>
 
+    
     <!-- Button mobile view to collapse sidebar menu -->
-    <div class="flex items-center">
-        <button class="text-white md:hidden">
-            <i class="mdi mdi-menu text-2xl"></i>
-        </button>
-    </div>
+    <!-- <div class="flex justify-start">
+        
+    </div> -->
+    
 
     <!-- Right(Notification) -->
     <div class="flex items-center">
@@ -22,7 +27,7 @@
             </a>
 
             <!-- Dropdown -->
-            <div class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20 hidden" id="user-menu">
+            <div class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 hidden" id="user-menu">
                 <p class="px-4 py-2 text-gray-700">Hi, Admin</p>
                 <a href="change-password.php" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Change Password</a>
                 <a href="logout.php" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Logout</a>
@@ -36,4 +41,10 @@
         const userMenu = document.getElementById('user-menu');
         userMenu.classList.toggle('hidden');
     });
+    // $(document).ready(function() {
+    // $('#user-menu-button').on('click', function() {
+    //     $('#user-menu').toggle();
+    // });
+    // });
+
 </script>
