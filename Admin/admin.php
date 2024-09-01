73c699815a93f1f44eaf849aea29c97a265b2d71
Nisarg Limbachiya <84617562+Nisarg077@users.noreply.github.com>
2024-09-01 20:08:16 +0530

    manual validation added for admin login and some changes

--- Admin/admin.php
+++ Admin/admin.php
@@ -13,58 +13,51 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- App title -->
         <title>News Portal | Dashboard</title>
-        <!-- App css -->
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css">
-    <script src="../assets/js/modernizr.min.js"></script>
-       
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css">
+        <script src="../assets/js/modernizr.min.js"></script>
     </head>
-    <body class="bg-gray-100 min-w-full sm:min-w-full">
+    <body class="bg-gray-100 min-h-screen">
+
         <?php include('../includes/topheader.php');?>
+
         <!-- Begin page -->
-        <div id="wrapper" class="flex flex-row min-h-screen">
-            
+        <div id="wrapper" class="flex flex-col md:flex-row min-h-screen">
 
-            <!-- ========== Left Sidebar Start ========== -->
-            <div id="toggleContent" >
-
+            <!-- Left Sidebar Start -->
+            <div id="toggleContent">
                 <?php include('../includes/leftsidebar.php');?>
             </div>
-            
             <!-- Left Sidebar End -->
 
-            <!-- ============================================================== -->
             <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="flex-grow z-0">
+            <div class="flex-grow p-6">
                 <!-- Start content -->
-                <div class="content p-6">
+                <div class="content">
                     <div class="container mx-auto">
                         <div class="mb-6">
                             <div class="flex justify-between items-center">
                                 <h4 class="text-2xl font-bold">Dashboard</h4>
-                                <ol class="breadcrumb flex space-x-1.5">
-                                    <li><a href="../index.php" class="text-gray-600">NewsPortal /</a></li>
-                                    <li><a href="#" class="text-gray-600">Admin /</a></li>
+                                <ol class="breadcrumb flex space-x-1.5 text-sm">
+                                    <li><a href="../index.php" class="text-gray-600 hover:text-gray-800">NewsPortal /</a></li>
+                                    <li><a href="#" class="text-gray-600 hover:text-gray-800">Admin /</a></li>
                                     <li class="text-gray-500">Dashboard</li>
                                 </ol>
                             </div>
                         </div>
-                        <!-- end row -->
 
+                        <!-- Cards Row 1 -->
                         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                             <a href="./manage-categories.php" class="block bg-white p-6 rounded-lg shadow">
                                 <div class="flex justify-between items-center space-x-4">
-                                    <!-- <i class="fa-solid fa-chart-simple text-4xl text-gray-600"></i> -->
                                     <div>
                                         <p class="text-gray-600 uppercase font-semibold">Categories Listed</p>
                                         <?php 
                                             $query = mysqli_query($con, "select * from tblcategory where Is_Active=1");
                                             $countcat = mysqli_num_rows($query);
-                                            ?>
+                                        ?>
                                         <h2 class="text-2xl font-bold"><?php echo htmlentities($countcat); ?></h2>
                                     </div>
                                     <i class="fa-solid fa-list text-4xl text-gray-600"></i>
@@ -73,7 +66,6 @@
 
                             <a href="manage-subcategories.php" class="block bg-white p-6 rounded-lg shadow">
                                 <div class="flex justify-between items-center space-x-4">
-                                    
                                     <div>
                                         <p class="text-gray-600 uppercase font-semibold">Listed Subcategories</p>
                                         <?php 
@@ -93,15 +85,15 @@
                                         <?php 
                                             $query = mysqli_query($con, "select * from tblposts where Is_Active=1");
                                             $countposts = mysqli_num_rows($query);
-                                            ?>
+                                        ?>
                                         <h2 class="text-2xl font-bold"><?php echo htmlentities($countposts); ?></h2>
                                     </div>
                                     <i class="fa-solid fa-newspaper text-4xl text-gray-600"></i>
                                 </div>
                             </a>
                         </div>
-                        <!-- end row -->
 
+                        <!-- Cards Row 2 -->
                         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
                             <a href="trash-posts.php" class="block bg-white p-6 rounded-lg shadow">
                                 <div class="flex justify-between items-center space-x-4">
@@ -110,20 +102,18 @@
                                         <?php 
                                             $query = mysqli_query($con, "select * from tblposts where Is_Active=0");
                                             $countposts = mysqli_num_rows($query);
-                                            ?>
+                                        ?>
                                         <h2 class="text-2xl font-bold"><?php echo htmlentities($countposts); ?></h2>
                                     </div>
                                     <i class="fa-solid fa-dumpster text-4xl text-gray-600"></i>
                                 </div>
                             </a>
                         </div>
-                    </div> <!-- container -->
-                </div> <!-- content -->
+                    </div>
+                </div>
             </div>
-            <!-- ============================================================== -->
             <!-- End Right content here -->
-            <!-- ============================================================== -->
-            
+
             <!-- Right Sidebar -->
             <div class="fixed inset-0 bg-gray-800 bg-opacity-50 z-50 hidden" id="right-sidebar">
                 <div class="bg-white w-64 p-4">
@@ -150,35 +140,27 @@
                         </div>
                     </div>
                 </div>
-                
             </div>
-            <!-- /Right-bar -->
+            <!-- /Right Sidebar -->
+
         </div>
         <!-- END wrapper -->
+
         <?php include('../includes/footer.php');?>
+
+        <script src="../assets/js/jquery.min.js"></script>
+        <script src="../assets/js/jquery.core.js"></script>
+        <script src="../assets/js/jquery.app.js"></script>
+
         <script>
-            var resizefunc = [];
             document.getElementById('close-sidebar').addEventListener('click', function() {
                 document.getElementById('right-sidebar').classList.add('hidden');
             });
+            document.getElementById('toggleButton').addEventListener('click', function() {
+                document.getElementById('toggleContent').classList.toggle('hidden');
+            });
         </script>
 
-
-        <!-- jQuery  -->
-        <script src="../assets/js/jquery.min.js"></script>
-    <script src="../assets/js/jquery.core.js"></script>
-    <script src="../assets/js/jquery.app.js"></script>
-
-        
-        </script>
-        <script>
-        document.getElementById('close-sidebar').addEventListener('click', function() {
-            document.getElementById('right-sidebar').classList.add('hidden');
-        });
-        document.getElementById('toggleButton').addEventListener('click', function() {
-            document.getElementById('toggleContent').classList.toggle('hidden');
-        });
-    </script>
     </body>
 </html>
 <?php } ?>
--- index.php
+++ index.php
@@ -18,174 +18,172 @@
     <title>News Everyday</title>
 
     <style>
-    .swiper-pagination-bullet {
-    background-color: lightyellow;
-    opacity: 0.8;
-  }
-  
-  .swiper-pagination-bullet-active {
-    background-color: orange; /* Optional: to distinguish the active bullet */
-  }
-    /* Adjustments for responsiveness */
-    @media (min-width: 640px) { /* sm */
-      .swiper-slide img {
-        height: 250px;
+      .swiper-pagination-bullet {
+        background-color: lightyellow;
+        opacity: 0.8;
       }
-    }
-    @media (min-width: 768px) { /* md */
-      .swiper-slide img {
-        height: 300px;
+      .swiper-pagination-bullet-active {
+        background-color: orange; /* Optional: to distinguish the active bullet */
       }
-    }
-    @media (min-width: 1024px) { /* lg */
-      .swiper-slide img {
-        height: 350px;
+      /* Adjustments for responsiveness */
+      @media (min-width: 640px) { /* sm */
+        .swiper-slide img {
+          height: 250px;
+        }
       }
-    }
-  </style>
+      @media (min-width: 768px) { /* md */
+        .swiper-slide img {
+          height: 300px;
+        }
+      }
+      @media (min-width: 1024px) { /* lg */
+        .swiper-slide img {
+          height: 350px;
+        }
+      }
+    </style>
   </head>
 
-  <body class="bg-gray-100">
+  <body class="bg-gray-100 w-screen">
 
     <!-- Navigation -->
     <?php include('./includes/header.php');?>
 
-    <div class="container px-2 mt-8">
-    <div class="swiper-container  w-full overflow-x-hidden px-4 relative">
-  <div class="swiper-wrapper w-full">
-    <?php 
-      $slider_sql = "SELECT id, PostTitle, PostImage FROM tblposts WHERE Is_Active = 1 ORDER BY PostingDate DESC LIMIT 5";
-      $slider_query = mysqli_query($con, $slider_sql);
+    <div class="container px-2 mt-8 mx-auto">
+      <div class="swiper-container w-full overflow-x-hidden px-4 relative">
+        <div class="swiper-wrapper w-full">
+          <?php 
+            $slider_sql = "SELECT id, PostTitle, PostImage FROM tblposts WHERE Is_Active = 1 ORDER BY PostingDate DESC LIMIT 5";
+            $slider_query = mysqli_query($con, $slider_sql);
 
-      while ($slider_row = mysqli_fetch_array($slider_query)) {
-    ?>
-    <div class="swiper-slide relative flex items-center justify-center">
-      <img src="Admin/postimages/<?php echo htmlentities($slider_row['PostImage']);?>" alt="<?php echo htmlentities($slider_row['PostTitle']);?>" class="w-full h-64 sm:h-80 md:h-96 lg:h-80 object-cover rounded-lg shadow-lg">
-      <div class="absolute inset-0 bg-black bg-opacity-50 flex items-end p-4 rounded-lg">
-        <div class="text-white">
-          <h3 class="text-lg sm:text-xl md:text-2xl font-bold mb-2"><?php echo htmlentities($slider_row['PostTitle']);?></h3>
-          <a href="news-details.php?nid=<?php echo htmlentities($slider_row['id']);?>" class="inline-block py-2 px-4 bg-blue-600 hover:bg-blue-800 text-white rounded-lg shadow-lg transition-colors duration-300">Read More &rarr;</a>
+            while ($slider_row = mysqli_fetch_array($slider_query)) {
+          ?>
+          <div class="swiper-slide relative flex items-center justify-center">
+            <img src="Admin/postimages/<?php echo htmlentities($slider_row['PostImage']);?>" alt="<?php echo htmlentities($slider_row['PostTitle']);?>" class="w-full h-64 sm:h-80 md:h-96 lg:h-80 object-cover rounded-lg shadow-lg">
+            <div class="absolute inset-0 bg-black bg-opacity-50 flex items-end p-4 rounded-lg">
+              <div class="text-white">
+                <h3 class="text-lg sm:text-xl md:text-2xl font-bold mb-2"><?php echo htmlentities($slider_row['PostTitle']);?></h3>
+                <a href="news-details.php?nid=<?php echo htmlentities($slider_row['id']);?>" class="inline-block py-2 px-4 bg-blue-600 hover:bg-blue-800 text-white rounded-lg shadow-lg transition-colors duration-300">Read More &rarr;</a>
+              </div>
+            </div>
+          </div>
+          <?php } ?>
         </div>
+
+        <!-- Add Pagination -->
+        <div class="swiper-pagination absolute bottom-0 w-full flex justify-center"></div>
+
+        <!-- Add Navigation -->
+        <div class="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 text-yellow-500"></div>
+        <div class="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 text-yellow-500"></div>
       </div>
     </div>
-    <?php } ?>
-  </div>
 
-  <!-- Add Pagination -->
-  <div class="swiper-pagination absolute bottom-0 w-full flex justify-center"></div>
-  
-  <!-- Add Navigation -->
-  <div class="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 text-yellow-500"></div>
-  <div class="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 text-yellow-500"></div>
-</div>
+    <!-- Page Content -->
+    <div class="container mx-auto px-4 mt-8">
+      <div class="flex flex-wrap">
 
-    
+        <!-- Blog Entries Column -->
+        <div class="w-full md:w-2/3 lg:w-3/4 mx-auto">
+          <!-- Blog Post -->
+          <?php 
+            if (isset($_GET['pageno'])) {
+                $pageno = $_GET['pageno'];
+            } else {
+                $pageno = 1;
+            }
+            $no_of_records_per_page = 8;
+            $offset = ($pageno-1) * $no_of_records_per_page;
 
-<!-- Page Content -->
-<div class="container mx-auto px-4 mt-8">
-  <div class="flex flex-wrap">
+            $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
+            $result = mysqli_query($con,$total_pages_sql);
+            $total_rows = mysqli_fetch_array($result)[0];
+            $total_pages = ceil($total_rows / $no_of_records_per_page);
 
-    <!-- Blog Entries Column -->
-    <div class="w-full md:w-2/3 lg:w-3/4 mx-auto">
-      <!-- Blog Post -->
-      <?php 
-        if (isset($_GET['pageno'])) {
-            $pageno = $_GET['pageno'];
-        } else {
-            $pageno = 1;
-        }
-          $no_of_records_per_page = 8;
-          $offset = ($pageno-1) * $no_of_records_per_page;
+            $sqlcmd = "SELECT tblposts.id as pid, tblposts.PostTitle as posttitle, tblposts.PostImage, tblcategory.CategoryName as category, tblcategory.id as cid, tblsubcategory.Subcategory as subcategory, tblposts.PostDetails as postdetails, tblposts.PostingDate as postingdate, tblposts.PostUrl as url 
+                       FROM tblposts 
+                       LEFT JOIN tblcategory ON tblcategory.id = tblposts.CategoryId 
+                       LEFT JOIN tblsubcategory ON tblsubcategory.SubCategoryId = tblposts.SubCategoryId 
+                       WHERE tblposts.Is_Active = 1 
+                       ORDER BY tblposts.id DESC  
+                       LIMIT $offset, $no_of_records_per_page";
+            $query = mysqli_query($con, $sqlcmd);
 
-          $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
-          $result = mysqli_query($con,$total_pages_sql);
-          $total_rows = mysqli_fetch_array($result)[0];
-          $total_pages = ceil($total_rows / $no_of_records_per_page);
+            while ($row = mysqli_fetch_array($query)) {
+          ?>
 
-          $sqlcmd = "SELECT tblposts.id as pid, tblposts.PostTitle as posttitle, tblposts.PostImage, tblcategory.CategoryName as category, tblcategory.id as cid, tblsubcategory.Subcategory as subcategory, tblposts.PostDetails as postdetails, tblposts.PostingDate as postingdate, tblposts.PostUrl as url 
-                     FROM tblposts 
-                     LEFT JOIN tblcategory ON tblcategory.id = tblposts.CategoryId 
-                     LEFT JOIN tblsubcategory ON tblsubcategory.SubCategoryId = tblposts.SubCategoryId 
-                     WHERE tblposts.Is_Active = 1 
-                     ORDER BY tblposts.id DESC  
-                     LIMIT $offset, $no_of_records_per_page";
-          $query = mysqli_query($con, $sqlcmd);
+          <div class="relative flex flex-col w-full max-w-3xl mx-auto rounded break-words border bg-white border-1 border-black mb-4">
+            <img class="w-full rounded-t" src="Admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+            <div class="flex-auto p-6">
+              <h2 class="mb-3 text-2xl font-bold"><?php echo htmlentities($row['posttitle']);?></h2>
+              <p>
+                <a class="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-gray-600 text-white" href="category.php?catid=<?php echo htmlentities($row['cid'])?>"><?php echo htmlentities($row['category']);?></a>
+                <a class="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-gray-600 text-white"><?php echo htmlentities($row['subcategory']);?></a>
+              </p>
+              <a href="news-details.php?nid=<?php echo htmlentities($row['pid']);?>" class="inline-block mt-3 py-1 px-3 text-white bg-blue-900 rounded hover:bg-blue-700">Read More &rarr;</a>
+            </div>
+            <div class="py-3 px-6 bg-gray-200 border-t border-gray-300 text-gray-700">
+              Posted on <?php echo htmlentities($row['postingdate']);?>
+            </div>
+          </div>
 
-          while ($row = mysqli_fetch_array($query)) {
-      ?>
+          <?php } ?>
 
-      <div class="relative flex flex-col w-full max-w-3xl mx-auto rounded break-words border bg-white border-1 border-black mb-4">
-        <img class="w-full rounded-t" src="Admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
-        <div class="flex-auto p-6">
-          <h2 class="mb-3 text-2xl font-bold"><?php echo htmlentities($row['posttitle']);?></h2>
-          <p>
-            <a class="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-gray-600 text-white" href="category.php?catid=<?php echo htmlentities($row['cid'])?>"><?php echo htmlentities($row['category']);?></a>
-            <a class="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-gray-600 text-white"><?php echo htmlentities($row['subcategory']);?></a>
-          </p>
-          <a href="news-details.php?nid=<?php echo htmlentities($row['pid']);?>" class="inline-block mt-3 py-1 px-3 text-white bg-blue-900 rounded hover:bg-blue-700">Read More &rarr;</a>
+          <!-- Pagination -->
+          <ul class="flex justify-center mt-4 mb-4 space-x-1">
+            <li class="page-item">
+              <a href="?pageno=1" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">First</a>
+            </li>
+            <li class="<?php if ($pageno <= 1) { echo 'opacity-50 cursor-not-allowed'; } ?> page-item">
+              <a href="<?php if ($pageno > 1) { echo "?pageno=" . ($pageno - 1); } else { echo '#'; } ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Prev</a>
+            </li>
+            <li class="<?php if ($pageno >= $total_pages) { echo 'opacity-50 cursor-not-allowed'; } ?> page-item">
+              <a href="<?php if ($pageno < $total_pages) { echo "?pageno=" . ($pageno + 1); } else { echo '#'; } ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Next</a>
+            </li>
+            <li class="page-item">
+              <a href="?pageno=<?php echo $total_pages; ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Last</a>
+            </li>
+          </ul>
+
         </div>
-        <div class="py-3 px-6 bg-gray-200 border-t border-gray-300 text-gray-700">
-          Posted on <?php echo htmlentities($row['postingdate']);?>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="w-full md:w-1/3 lg:w-1/4 px-4 mt-8 md:mt-0">
+          <?php include('./includes/sidebar.php');?>
         </div>
+
       </div>
-
-      <?php } ?>
-
-      <!-- Pagination -->
-      <ul class="flex justify-center mt-4 mb-4 space-x-1">
-        <li class="page-item">
-          <a href="?pageno=1" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">First</a>
-        </li>
-        <li class="<?php if ($pageno <= 1) { echo 'opacity-50 cursor-not-allowed'; } ?> page-item">
-          <a href="<?php if ($pageno > 1) { echo "?pageno=" . ($pageno - 1); } else { echo '#'; } ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Prev</a>
-        </li>
-        <li class="<?php if ($pageno >= $total_pages) { echo 'opacity-50 cursor-not-allowed'; } ?> page-item">
-          <a href="<?php if ($pageno < $total_pages) { echo "?pageno=" . ($pageno + 1); } else { echo '#'; } ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Next</a>
-        </li>
-        <li class="page-item">
-          <a href="?pageno=<?php echo $total_pages; ?>" class="block py-2 px-3 leading-normal text-blue-700 bg-white border border-gray-200 hover:text-blue-800 hover:bg-gray-200">Last</a>
-        </li>
-      </ul>
+      <!-- /.row -->
 
     </div>
+    <!-- /.container -->
 
-    <!-- Sidebar Widgets Column -->
-    <div class="w-full md:w-1/3 lg:w-1/4 px-4">
-      <?php include('./includes/sidebar.php');?>
-    </div>
+    <!-- Footer -->
+    <?php include('./includes/footer.php');?>
 
-  </div>
-  <!-- /.row -->
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
-</div>
-<!-- /.container -->
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        autoplay: {
+          delay: 2800,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    </script>
 
-<!-- Footer -->
-<?php include('./includes/footer.php');?>
-
-<!-- Swiper JS -->
-<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-
-  <!-- Initialize Swiper -->
-  <script>
-  var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    autoplay: {
-      delay: 2800,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-</script>
-
-</body>
+  </body>
 </html>
